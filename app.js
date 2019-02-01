57ba1e293ed5c57038fdd60a1685ccf5b4223284
Mkknowledge <kmayur2124@gmail.com>
2019-02-01 11:52:09 +0530

    Changing The Active Player!

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activeScore;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 //to keep track on currently playing player :
-activeScore = 1;
+activePlayer = 0;
 
 
 document.querySelector('.dice').style.display = 'none';
@@ -35,7 +35,24 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.src = 'dice-' + dice + '.png';
     
     //3.Update the round score IF the rolled number was not 1 
-    
+    if(dice !== 1) {
+        //Add Score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        
+        document.querySelector('.dice').style.display = 'none';
+        
+    }
     
     
 });
